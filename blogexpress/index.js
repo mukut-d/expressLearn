d1a504bb705d15bae5c4954f3a5e09b8625851fd
mukut-d <mukut.m.das2000@gmail.com>
2025-03-09 13:54:22 +0530

    used express-handlebars pkg

--- blogexpress/index.js
+++ blogexpress/index.js
@@ -1,7 +1,17 @@
+const exphbs = require("express-handlebars");
 const express = require("express");
 const path = require("path");
+const port = 3001;
+
 const app = express();
-const port = 3000;
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.use(express.static(path.join(__dirname, "static")));
 app.use("/", require(path.join(__dirname, "routes/blog.js")));
